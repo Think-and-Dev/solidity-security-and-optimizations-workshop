44a33dc4d1fa6193e5775a2513ed1b735dd65e3e
Lucas Marc <lucas@thinkanddev.com>
2023-09-26 23:12:42 -0300

    feat: added safeMath version and some initial project files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # solidity-security-and-optimizations-workshop
- This repo contains some support code and examples that will be used on a workshop about security and optimizations on Solidity
+
+This repo contains some support code and examples that will be used on a workshop about security and optimizations on Solidity
--- contracts/5_overflow/Contract_V0.7.sol
+++ contracts/5_overflow/Contract_V0.7.sol
@@ -1,17 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
  * Overflows playground
  */
 contract Overflow07 {
-    uint8 public number;
-    uint8 MAX_INT = 2 ** 8 - 1;
+    using SafeMath for uint256;
+    uint256 public number;
+    uint256 MAX_INT = 2 ** 256 - 1;
 
     /**
      * Causes an overflow but it won't fail on Solidity < 0.8
      */
     function overflow() public {
         number = MAX_INT + 2;
+    }
+
+    /**
+     * This version will be safe even on Solidity < 0.8, by using the safe math library
+     */
+    function safeOverflow() public {
+        number = MAX_INT.add(2);
     }
 }
